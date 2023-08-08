0aea1e0a5f084bb2cfb911ed27ba165126a0e1e2
rifqyarifani <rifqy.arifani44@gmail.com>
2023-08-08 23:36:16 +0800

    first commit

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,4 +1,5 @@
 const productData = require("../models/data.json");
+const { postNew } = require("../models");
 
 const allProducts = (req, res) => {
   res.send({ data: productData.products });
@@ -12,4 +13,69 @@ const productById = (req, res) => {
   });
 };
 
-module.exports = { allProducts, productById };
+const createProduct = async (req, res) => {
+  const idUser = req.user.id;
+  const { title, body } = req.body;
+
+  if (!idUser || !title || !body) {
+    return res.status(400).send({
+      message: "Some field must be filled, cannot be empty",
+    });
+  }
+
+  const input = await postNew.create({
+    user_id: idUser,
+    title: title,
+    body: body,
+  });
+
+  return res.status(201).send({
+    message: "post created",
+  });
+};
+
+const updateProduct = async (req, res) => {
+  try {
+    const { id, title, body } = req.body;
+
+    const updatedData = await postNew.update(
+      {
+        title: title,
+        body: body,
+      },
+      { where: { id: id } }
+    );
+    res.status(201).send({
+      message: "user updated",
+    });
+  } catch (error) {
+    res.send({
+      message: "error occured",
+      data: error,
+    });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const deletedProduct = await postNew.destroy({ where: { id: id } });
+    res.status(200).send({
+      message: "your product has been deleted",
+      data: deletedProduct,
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: "error occured",
+      data: error,
+    });
+  }
+};
+
+module.exports = {
+  allProducts,
+  productById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -2,6 +2,9 @@ const express = require("express");
 const {
   allProducts,
   productById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
 } = require("../controllers/product.controller");
 const { verifyToken } = require("../middlewares/verifytoken");
 
@@ -9,5 +12,8 @@ const router = express.Router();
 
 router.get("/", verifyToken, allProducts);
 router.get("/:id", verifyToken, productById);
+router.post("/createproduct", verifyToken, createProduct);
+router.put("/updateproduct", verifyToken, updateProduct);
+router.delete("/deleteproduct", verifyToken, deleteProduct);
 
 module.exports = router;
